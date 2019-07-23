d3ceef31daeee167191a1fbd3dc5c5ba1fc981a8
Lukasz Wojcik (PGS Software) <lwojcik@pgs-soft.com>
2019-07-23 10:58:26 +0200

    add proxy middlewares

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,15 @@ import * as path from 'path';
 import * as bodyParser from "body-parser";
 import * as config from "./config";
 import userRouter from './areas/users/controller';
+import * as proxy from 'http-proxy-middleware';
+
 var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+app.use(proxy('http://localhost:8000/api/orders'));
+app.use(proxy('http://localhost:8001/api/cdn'));
+app.use(proxy('http://some.external.api.test:9002/api/external'));
 
 app.use('/api/users', userRouter);
 
